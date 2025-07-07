65d45c4148bbd853bb4bff6e53a9b3027774de76
AbhiganBanerjee <abhiganbanerjee1@gmail.com>
2025-07-07 22:32:13 +0530

    First Commit

--- src/main/java/in/spring/MarvelRivalsSbApiApplication.java
+++ src/main/java/in/spring/MarvelRivalsSbApiApplication.java
@@ -1,4 +1,4 @@
-/*This is the Start class of our API*/
+/*This is the Start class of our API a little change here*/
 package in.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
